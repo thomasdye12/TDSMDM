85152b3517a1c3b98baa025ff5b06d2f34a68ca4
thomasdye12 <thomas@thomasdye.net>
2025-04-25 13:04:51 +0100

    updated routes and new APNS for IOS app

---
+++ src/router.js
@@ -0,0 +1,37 @@
+// router.tsx
+import React from 'react';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+
+import AppLayout from './AppLayout';
+import DeviceList from './components/DeviceList';
+import DeviceDetails from './components/DeviceDetails';
+import AppList from './components/apps/AppList';
+import Events from './components/events/eventQueue';
+import ProfileList from './components/Profiles/ProfileList';
+import ProfileEdit from './components/Profiles/edit/ProfileEdit';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<AppLayout />}>
+      <Route index element={<Navigate to="/devices" replace />} />
+      <Route path="devices" element={<DeviceList />} />
+      <Route path="devices/:udid" element={
+        <>
+         <DeviceList />
+       <DeviceDetails />
+      </>
+      } />
+      <Route path="apps" element={<AppList />} />
+      <Route path="profiles" element={<ProfileList />} />
+      <Route path="profiles/:profileid/edit" element={<ProfileEdit />} />
+      <Route path="events" element={<Events />} />
+    </Route>
+  )
+);
+
+export default router;
